9fee7c95a416f27756f60ded7d8e3d3b009faa76
riteshprk <reach2ritesh@gmail.com>
2020-11-08 12:27:19 +0000

    atyle added

--- src/components/CopyCat.js
+++ src/components/CopyCat.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import {divStyles} from '../styles.js';
+import {imgStyles} from '../styles.js';
 
 const images = {
   copycat: 'https://content.codecademy.com/courses/React/react_photo_copycat.png',
@@ -11,9 +12,9 @@ export class CopyCat extends React.Component {
  
     render(){
     return (
-      <div>
-        <h1>Copy Cat</h1>
-        <img 
+      <div style={divStyles}>
+        <h1 style={{marginBottom: 80}}>Copy Cat</h1>
+        <img style={imgStyles}
           alt='cat'
           src={this.props.xvalue.copying ? images.copycat : images.quietcat}
           onClick={this.props.xvalue.toggleTape}
